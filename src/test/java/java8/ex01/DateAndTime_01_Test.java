c501c1618481489c7d1fdcea82481a988a045fb1
etbourreau <et.bourreau@laposte.net>
2017-07-12 09:50:57 +0100

    java8 dates&periods

--- src/test/java/java8/ex01/DateAndTime_01_Test.java
+++ src/test/java/java8/ex01/DateAndTime_01_Test.java
@@ -18,14 +18,15 @@ public class DateAndTime_01_Test {
     public void test_date() {
 
         // TODO modifier les paramètres pour que le test soit passant
-        Date date = new Date(0, 0, 0, 0,0,0);
+        Date date = new Date(117, 2, 24, 1 ,2 ,3);
 
-        assertThat(date.toString(), is("Fri Mar 24 01:02:03 CET 2017"));
+        assertThat(date.toString(), is("Fri Mar 24 01:02:03 GMT 2017"));
 
         // une date est mutable... :-(
         // TODO modifier le jour de la date pour que le test soit passant
+        date = new Date(117, 2, 25, 1, 2, 3);
 
-        assertThat(date.toString(), is("Sat Mar 25 01:02:03 CET 2017"));
+        assertThat(date.toString(), is("Sat Mar 25 01:02:03 GMT 2017"));
     }
 
     // Vous vous souvenez de java.util.Calendar (Java 1.1) ?
@@ -33,15 +34,17 @@ public class DateAndTime_01_Test {
     @Test
     public void test_calendar() throws Exception {
         Calendar calendar = Calendar.getInstance();
+        calendar.set(2017, 2, 24, 1, 2, 3);
 
         // TODO modifier l'objet calendar pour que le test soit passant
 
-        assertThat(calendar.getTime().toString(), is("Fri Mar 24 01:02:03 CET 2017"));
+        assertThat(calendar.getTime().toString(), is("Fri Mar 24 01:02:03 GMT 2017"));
 
         // calendar est mutable... :-(
         // TODO modifier l'objet calendar pour que le test soit passant
+        calendar.set(Calendar.DAY_OF_MONTH, 25);
 
-        assertThat(calendar.getTime().toString(), is("Sat Mar 25 01:02:03 CET 2017"));
+        assertThat(calendar.getTime().toString(), is("Sat Mar 25 01:02:03 GMT 2017"));
     }
 
 
--- src/test/java/java8/ex07/DateAndTime_07_Test.java
+++ src/test/java/java8/ex07/DateAndTime_07_Test.java
@@ -2,9 +2,11 @@ package java8.ex07;
 
 import org.junit.Test;
 
-import java.time.DayOfWeek;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Month;
+import java.time.ZoneId;
 import java.util.Date;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -19,13 +21,17 @@ public class DateAndTime_07_Test {
     public void test_date_to_localdate() throws Exception {
 
         // TODO Créer une date Java 1 (12/02/2017)
-        Date date = null;
+        @SuppressWarnings("deprecation")
+		Date date = new Date(117, 1, 12);
 
         // TODO transformer la date en Instant
+        Instant dateInstant = date.toInstant();
 
         // TODO transformer la date en LocalDate
 
-        LocalDate result = null;
+        LocalDate result = LocalDate.from(LocalDateTime.ofInstant(dateInstant, ZoneId.systemDefault()));
+        //or
+        result = dateInstant.atZone(ZoneId.systemDefault()).toLocalDate();
 
         // TODO valoriser les différentes variables afin de rendre le test passant
 
